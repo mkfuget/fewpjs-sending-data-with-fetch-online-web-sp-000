8906e5edbbeea250f79e7f98b93746a8b434e33d
Michael Fuget <michaelfuget@gmail.com>
2021-02-03 04:05:15 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -7,12 +7,19 @@ function postData()
   };
 
   configurationObject = {
-  method: "POST",
-  headers: {
-    "Content-Type": "application/json",
-    "Accept": "application/json"
-  },
-  body: JSON.stringify(formData)
-};
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      "Accept": "application/json"
+    },
+    body: JSON.stringify(formData)
+  };
+  fetch("http://localhost:3000/dogs", configObj)
+    .then(function(response) {
+      return response.json();
+    })
+    .then(function(object) {
+      console.log(object);
+    });
 
 }
