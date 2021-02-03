855e932ceb3555793fb3f2974f9f449168730175
Michael Fuget <michaelfuget@gmail.com>
2021-02-03 04:04:48 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Add your code here
+function postData()
+{
+  let formData = {
+    dogName: "Byron",
+    dogBreed: "Poodle"
+  };
+
+  configurationObject = {
+  method: "POST",
+  headers: {
+    "Content-Type": "application/json",
+    "Accept": "application/json"
+  },
+  body: JSON.stringify(formData)
+};
+
+}
