5c4bb17b412eda066be203d98dc75bb7e220843a
Michael Fuget <michaelfuget@gmail.com>
2021-02-03 08:19:38 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -21,6 +21,7 @@ function submitData(name, email)
       return response.json();
     })
     .then(function(object) {
+      console.log(object)
       h1.innerHTML = object.id
       body.appendChild(h1);
     })
